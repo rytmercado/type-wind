c4e2bc20891cd1e815654ebe5ff8e7de87cfad91
Ry Mercado <rytmercado@gmail.com>
2022-10-20 15:24:27 -0600

    working centered counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello World!
-    </h1>
+  <div className="grid h-screen place-items-center">
+    <div className="flex">
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <h1 className="text-3xl font-bold p-8">
+          {counter}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+    </div>
+  </div>
   );
 }
 
