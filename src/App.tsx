2c6cc3f1eb69a884dfb8051240650e452d245f36
Ry Mercado <rytmercado@gmail.com>
2022-10-20 15:31:40 -0600

    cleaning up typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const increment = (num : number) => {
+
+    setCounter(counter + num);
+  }
+  
+
   return (
   <div className="grid h-screen place-items-center">
     <div className="flex">
-      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <button onClick={() => increment(-1)}>-</button>
       <h1 className="text-3xl font-bold p-8">
           {counter}
       </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => increment(1)}>+</button>
     </div>
   </div>
   );
