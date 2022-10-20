35c634907c289b71964c4b381bc3d714e96809d7
Ry Mercado <rytmercado@gmail.com>
2022-10-20 15:46:41 -0600

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+	const [counter, setCounter] = useState(0);
 
-  const increment = (num : number) => setCounter(counter + num);
+	const increment = (num: number) => {
+		setCounter(counter + num);
+	};
 
-  return (
-  <div className="grid h-screen place-items-center">
-    <div className="flex">
-      <button onClick={() => increment(-1)}>-</button>
-      <h1 className="text-3xl font-bold p-8">
-          {counter}
-      </h1>
-      <button onClick={() => increment(1)}>+</button>
-    </div>  
-  </div>
-  );
+	return (
+		<div className='grid h-screen place-items-center'>
+			<div className='flex'>
+				<button onClick={() => {
+					increment(-1);
+				}}>-</button>
+				<h1 className='text-3xl font-bold p-8'>
+					{counter}
+				</h1>
+				<button onClick={() => {
+					increment(1);
+				}}>+</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
